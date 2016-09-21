afb833f86761cb841eba97a6be26ac0ddd069b5d
Pablo Colaiacovo <pcolaiacovo@bluetubeinc.com>
2016-09-21 11:55:10 -0300

    Added first controller rendering

---
+++ ScTraining/Controllers/TrainingController.cs
@@ -0,0 +1,26 @@
+﻿using Glass.Mapper.Sc;
+using ScTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScTraining.Controllers
+{
+    public class TrainingController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var context = new SitecoreContext();
+            var model = context.GetHomeItem<Home>();
+            return PartialView("~/Views/Training/Index.cshtml", model);
+        }
+
+        public ActionResult Carousel()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScTraining/Models/Home.cs
@@ -0,0 +1,17 @@
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScTraining.Models
+{
+    [SitecoreType(AutoMap = true, TemplateId = "{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}")]
+    public class Home
+    {
+        [SitecoreField("Title")]
+        public virtual string Title { get; set; }
+        [SitecoreField("Text", Setting = Glass.Mapper.Sc.Configuration.SitecoreFieldSettings.RichTextRaw)]
+        public virtual string Text { get; set; }
+    }
+}
